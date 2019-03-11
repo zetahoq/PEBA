fc57539d54e5f890df077d5a5d13d7f09f407b02
hoq <34850716+zetahoq@users.noreply.github.com>
2019-03-11 10:56:35 -0700

    Fixed downloaded file locations

--- var/lib/GeoIP/download.sh
+++ var/lib/GeoIP/download.sh
@@ -2,11 +2,9 @@
 
 
 rm Geo*.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
-gunzip GeoLiteCity.dat.gz
-gunzip GeoIP.dat.gz
-gunzip GeoIPASNum.dat.gz
-
-
+wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
+wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz
+gunzip GeoLite2-City.tar.gz
+gunzip GeoLite2-Country.tar.gz
+gunzip GeoLite2-ASN.tar.gz
